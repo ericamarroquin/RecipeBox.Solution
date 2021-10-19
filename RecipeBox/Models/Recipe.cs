60a646e90f12df4444fbdbac943539d6cc25157d
EricaMarroquin <ericamarroquin03@gmail.com>
2021-10-19 11:52:31 -0700

    add ingredients and instructions property to recipe database
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -11,6 +11,10 @@ namespace RecipeBox.Models
 
     public int RecipeId { get; set; }
     public string Description { get; set; }
+
+    public string Ingredients { get; set; }
+
+    public string Instructions { get; set; }
     public virtual ApplicationUser User { get; set; }
 
     public virtual ICollection<RecipeTag> JoinEntities { get; }
